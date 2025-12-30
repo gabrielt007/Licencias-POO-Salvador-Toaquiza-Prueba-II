264541c5f6040f6de3537ce418f860b448d28c66
Gabriel <gabrieltoaquiza.saenz@gmail.com>
2025-12-29 19:50:49 -0500

    Primeros forms - sujeto a cambios

--- src/View/Login.java
+++ src/View/Login.java
@@ -1,4 +1,10 @@
 package View;
 
+import javax.swing.*;
+
 public class Login {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton ingresarButton;
+    private JPanel LoginPane;
 }
--- src/View/Registro.java
+++ src/View/Registro.java
@@ -1,4 +1,13 @@
 package View;
 
+import javax.swing.*;
+
 public class Registro {
+    private JTextField txtCedula;
+    private JTextField txtNombre;
+    private JTextField txtEdad;
+    private JComboBox ComBxTipoLicencia;
+    private JPasswordField txtPassword;
+    private JPanel Registro;
+    private JButton registrarButton;
 }
