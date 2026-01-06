eab5905d23e4f8a626c55e171d8727102562d5da
Gabriel <gabrieltoaquiza.saenz@gmail.com>
2026-01-05 23:27:27 -0500

    Primeros forms - sujeto a cambios - ventanas de datos 1

---
+++ src/View/Requisitos.java
@@ -0,0 +1,16 @@
+package View;
+
+import javax.swing.*;
+
+public class Requisitos {
+    private JCheckBox checkBox1;
+    private JCheckBox checkBox2;
+    private JCheckBox checkBox3;
+    private JCheckBox checkBox4;
+    private JCheckBox checkBox5;
+    private JCheckBox checkBox6;
+    private JFormattedTextField formattedTextField1;
+    private JButton aprobarButton;
+    private JButton rechazarButton;
+    private JPanel Requisitos;
+}
