0b4d151090cfa0e3ea20617ae9f2f8646413ea52
Gabriel <gabrieltoaquiza.saenz@gmail.com>
2026-01-05 23:16:49 -0500

    Primeros forms - sujeto a cambios -admin

--- src/View/PerfilAdmin.java
+++ src/View/PerfilAdmin.java
@@ -3,4 +3,11 @@ package View;
 import javax.swing.*;
 
 public class PerfilAdmin extends JFrame {
+    private JPanel PerfilAdmin;
+    private JButton registrarButton;
+    private JButton tramitesButton;
+    private JButton generarButton;
+    private JButton usuariosButton;
+    private JButton reportesButton;
+    private JButton cerrarSesionButton;
 }
