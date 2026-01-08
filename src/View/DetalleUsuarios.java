be516b0d7591f8ecb85ee6837f78de4a29687300
Bryan13092005 <bryan.salvador@epn.edu.ec>
2026-01-08 00:20:18 -0500

    detalles usuario

---
+++ src/View/DetalleUsuarios.java
@@ -0,0 +1,12 @@
+package View;
+
+import javax.swing.*;
+
+public class DetalleUsuarios {
+    private JTable datosPersonalesTable;
+    private JTable requisitosTable;
+    private JTable notasTable;
+    private JScrollPane datosPersonales;
+    private JScrollPane requisitos;
+    private JScrollPane notas;
+}
